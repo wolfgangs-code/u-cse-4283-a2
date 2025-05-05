8dfe9b287a40efbc9afcb93f69cd221afb52bb68
Wolfgang de Groot <wolfgang@wolfgang.space>
2025-05-05 12:11:37 -0500

    Create test_bmi.py

---
+++ tests/test_bmi.py
@@ -0,0 +1,24 @@
+import pytest
+
+# Core mathematical tests
+def test_bmi_positives():
+    assert False
+
+def test_bmi_zeros():
+    assert False
+
+def test_bmi_negatives():
+    assert False
+
+# BMI calculation tests
+def test_bmi_underweight():
+    assert False
+
+def test_bmi_healthy():
+    assert False
+
+def test_bmi_overweight():
+    assert False
+
+def test_bmi_obese():
+    assert False
